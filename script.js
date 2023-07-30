1ac30d0dd0a9165f6937ee57b251d362b585e5e2
Kacper Mazur <109112557+mazur-kacper@users.noreply.github.com>
2023-07-30 21:09:02 +0200

    Add button that makes a new grid with provided size

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ for (let i = 0; i < 256; i++) {
       "background-color: black; height: 30px; width: 30px; ";
   });
 }
+
+const gridSizeButton = document.querySelector("#grid-size-btn");
+
+gridSizeButton.addEventListener("click", (event) => {
+  let size = prompt("How many squares per side should there be?");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  let fields = size * size;
+  for (let i = 0; i < fields; i++) {
+    const squareDiv = document.createElement("div");
+    container.appendChild(squareDiv);
+    squareDiv.style.cssText = `background-color: white; height: ${
+      480 / size
+    }px; width: ${480 / size}px; `;
+    squareDiv.addEventListener("mouseover", (event) => {
+      squareDiv.style.cssText = `background-color: black; height: ${
+        480 / size
+      }px; width: ${480 / size}px; `;
+    });
+  }
+});
